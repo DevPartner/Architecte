77b9df1ebd1f91bd9a8ecb5b2aa68663ed17c9f4
Konstantin Ivinsky <Konstantin_Ivinsky@epam.com>
2024-07-29 16:03:14 +0200

    L5 - Set up Identity Management System

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,71 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using IdentityModel;
+using System.Security.Claims;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        [
+            new ApiScope("catalogAPI", "Catalog API"),
+            new ApiScope("cartAPI", "Cart API")
+        ];
+
+    public static IEnumerable<Client> Clients
+        => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "catalogAPI", "cartAPI" }
+            },
+            new Client
+            {
+                ClientId = "ro.client",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"catalogAPI", "cartAPI"},
+            },
+        };
+
+    public static List<TestUser> GetUsers()
+    {
+        return new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "1",
+                Username = "manager",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim(JwtClaimTypes.Role, "Manager")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "2",
+                Username = "buyer",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim(JwtClaimTypes.Role, "Buyer")
+                }
+            }
+        };
+    }
+}
